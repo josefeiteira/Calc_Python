d9a7239686c6d0c0eb0b4bd0822e441dd339e247
josefeiteira <57048227+josefeiteira@users.noreply.github.com>
2019-11-16 21:23:24 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+"""Calculadora em Python - apenas números inteiros e as 4 operações básicas
+Tem um pequeno menu de ajuda quando solicitado e a opção de sair
+José Feiteira
+"""
+import re
+
+
+# Função Principal da aplicação
+def main():
+    #Expressão regular apenas aceita numeros inteiros
+    reg_expression = re.compile("^\\s*(\\d+\\s*[+|-|/]\\s*)*\\d+$")
+    print ("Se necessitar de ajuda, digite 'Ajuda'; Se quiser sair digite 'Sair'")
+    print("Insira a conta que quer realizar:")
+    while True:
+        user_input = input("->").strip().lower()
+        if user_input == "ajuda":
+            ajuda()
+        elif user_input == "sair":
+            break
+        elif reg_expression.match(user_input):
+             print(user_input.lstrip().rstrip() + " = " + str(eval(user_input)))
+        else:
+            print(user_input + " = " + str(eval(user_input)))
+            print("Expressão Inválida!")
+
+def ajuda():
+    print("Operações que pode realizar:")
+    print("Somar -> +")
+    print("Subtrair -> -")
+    print("Multiplicar -> *")
+    print("Dividir -> /")
+
+"""Este if indica que só vai correr o main se for módulo principal"""
+if __name__ == '__main__':
+    main()
